6185eebda28e33c9cfc0ca7cfb1c886dfb514386
Sushobhan Nayak <sushobhannayak17@gmail.com>
2023-06-28 11:49:43 +0530

    Splash screen and icon changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/splash.dart';
+import 'package:hello_world/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Spalsh(),
+      home: Homepage(),
     );
   }
 }
